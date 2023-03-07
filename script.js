a8c8f3291e033ef274568f62f6338ad6b0cb9e8e
Barry <mbsampson89@gmail.com>
2023-03-06 21:19:48 -0800

    Save website progress

---
+++ script.js
@@ -0,0 +1,30 @@
+let hideCoverExp = false;
+let currentExpTile;
+
+function showExperience(expNum) {
+  let cover = document.getElementById("experience-cover");
+  let exp1 = document.getElementById("experience-1");
+  let exp2 = document.getElementById("experience-2");
+  let exp3 = document.getElementById("experience-3");
+  let exp4 = document.getElementById("experience-4");
+  let expList = [exp1, exp2, exp3, exp4];
+
+  // Check to see if the user is trying to enable an already displaying tile
+  if(currentExpTile !== expNum) {
+    currentExpTile = expNum;
+    // Remove the 'hidden' class from the selected tile and add it to all others
+    for( let i = 0; i < expList.length; i++ ) {
+      if(i === expNum) { 
+        expList[i].classList.toggle("hidden");
+      } else {
+        expList[i].classList.add("hidden");
+      }
+    }
+    
+    // Hides the cover after a user interacts with an experience tile
+    if(hideCoverExp === false) {
+      cover.classList.toggle("hidden");
+      hideCoverExp = true;
+    }
+  } 
+} 
